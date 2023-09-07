67724efd724d0e69263d4b19db169a9c54b99b52
ajay3568yadav <89062459+ajay3568yadav@users.noreply.github.com>
2023-09-07 16:55:44 -0700

    Add files via upload

---
+++ fifa/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  fifa
+//
+//  Created by Ajay Yadav on 11/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+   
+    
+    @State private var searchText: String=""
+    
+    var searchResults:[Team]{
+        if searchText.isEmpty{
+            return teamsList
+        } else{
+            return teamsList.filter{ team in
+                team.name.contains(searchText)
+            }
+        }
+    }
+    
+    var body: some View {
+        NavigationView{
+            List{
+                ForEach(searchResults){team in
+                    NavigationLink(destination:TeamView(selectedTeam: team)){
+                        Text(team.flag+" "+team.name)
+                    }
+                }
+            }
+            .searchable(text: $searchText)
+            .navigationTitle("World Cup Teams")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fifa/fifaApp.swift
@@ -0,0 +1,17 @@
+//
+//  fifaApp.swift
+//  fifa
+//
+//  Created by Ajay Yadav on 11/17/22.
+//
+
+import SwiftUI
+
+@main
+struct fifaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
